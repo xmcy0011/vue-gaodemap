021b1b2e3a85489dfb5aa9a9c748ca40299547df
xmcy0011 <xmcy0011@sina.com>
2018-12-07 19:28:20 +0800

    新增初始项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="MapApp"></div>
+</template>
+
+<script>
+import AMap from "AMap";
+console.log(AMap);
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  mounted() {
+    const map = new AMap.Map("MapApp", {
+      zoom: 15,
+      center: [116.39, 39.9]
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#MapApp {
+  width: 800px;
+  height: 600px;
+}
+</style>
